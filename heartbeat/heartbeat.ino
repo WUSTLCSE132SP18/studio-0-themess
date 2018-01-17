86a74d47410b35013535f82ec8e75a6140e17eea
Adam Messer <adam.messer@wustl.edu>
2018-01-17 14:14:41 -0600

    This contains the hearbeat.ino file

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,24 @@
+    /* helloworld
+ *
+ * output simple hello message
+ *
+ */
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello, world!");
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  double n = millis();
+  delay(1000);
+  Serial.print(n/1000);
+  Serial.println(" seconds have elapsed."); 
+  digitalWrite(13, HIGH);
+  delay (1000);
+  digitalWrite(13, LOW);
+  Serial.print(n/1000 + 1);
+  Serial.println(" seconds have elapsed.");
+  Serial.println(n);
+}
